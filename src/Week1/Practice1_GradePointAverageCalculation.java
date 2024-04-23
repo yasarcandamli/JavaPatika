3b040dd85c605a8214ec9f5bc91076a525b5ff91
Yaşar Can Damlı <108733740+yasarcandamli@users.noreply.github.com>
2024-04-23 11:15:26 +0300

    Practice1_GradePointAverageCalculation classına yorum satırları eklenmiştir.

--- src/Week1/Practice1_GradePointAverageCalculation.java
+++ src/Week1/Practice1_GradePointAverageCalculation.java
@@ -5,8 +5,13 @@ import java.util.Scanner;
 public class Practice1_GradePointAverageCalculation {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        /*Derslerden alınan not değişkenlerinin tanımlanması (defining grade variables for courses):*/
+
         int gradeMath, gradePhysics, gradeChemistry, gradeTurkish, gradeHistory, gradeMusic;
 
+        /*Tanımlanan ders notu puanlarının kullanıcıdan istenmesi (requesting the defined course grade points from
+         the user):*/
         System.out.println("Matematik Sınav Puanını Giriniz: ");
         gradeMath = input.nextInt();
 
@@ -25,10 +30,14 @@ public class Practice1_GradePointAverageCalculation {
         System.out.println("Müzik Sınav Puanını Giriniz: ");
         gradeMusic = input.nextInt();
 
+        /*Puanların toplamını (gradeSum) ve ortalamasını (gradeAverage) tutacağımız değişkenlerin tanımlanması ve
+         hesaplanması (defining and calculating the variables that will hold the sum (gradeSum) and
+         average (gradeAverage) of the grade points):*/
         double gradeSum, gradeAverage;
         gradeSum = gradeMath + gradePhysics + gradeChemistry + gradeTurkish + gradeHistory + gradeMusic;
         gradeAverage = gradeSum / 6;
 
+        /*Program çıktısının oluşturulması ve kullanıcıya gösterilmesi:*/
         String message = (gradeAverage >= 60) ? "Sınıfı Geçti" : "Sınıfta Kaldı";
         System.out.println(message);
     }

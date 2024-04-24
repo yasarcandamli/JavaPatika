ffbea1c420e087d41c33dcf173488a9f9442e59b
yasarcandamli <yasarcandamli@gmail.com>
2024-04-24 10:13:26 +0300

    Aritmetik işlemler ve işlem önceliği (Arithmetic operations and operation precedence).

---
+++ src/Week1/Practice10_ArithmeticOperationsAndOperationPrecedence.java
@@ -0,0 +1,22 @@
+package Week1;
+
+import java.util.Scanner;
+
+public class Practice10_ArithmeticOperationsAndOperationPrecedence {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a, b, c, result;
+
+        System.out.print("1. Sayıyı Giriniz: ");
+        a = input.nextInt();
+
+        System.out.print("2. Sayıyı Giriniz: ");
+        b = input.nextInt();
+
+        System.out.print("3. Sayıyı Giriniz: ");
+        c = input.nextInt();
+
+        result = a + b * c - b;
+        System.out.println("İşlem sonucu: " + result);
+    }
+}

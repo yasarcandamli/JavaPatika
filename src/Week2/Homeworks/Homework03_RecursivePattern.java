fd24ec31ada55fd2afb864cfcb59e43e8ef9cffb
yasarcandamli <yasarcandamli@gmail.com>
2024-05-01 14:26:57 +0300

    Comment lines added.

--- src/Week2/Homeworks/Homework03_RecursivePattern.java
+++ src/Week2/Homeworks/Homework03_RecursivePattern.java
@@ -3,23 +3,23 @@ package Week2.Homeworks;
 import java.util.Scanner;
 
 public class Homework03_RecursivePattern {
-    static boolean status = false;
+    static boolean status = true;
     static int inputNumber;
     static int pattern(int methodNumber) {
-        if (status) {
-            if (methodNumber == inputNumber) {
-                System.out.print(methodNumber + " ");
-                return methodNumber;
-            }
-            System.out.print(methodNumber + " ");
-            return pattern(methodNumber + 5);
-        } else {
+        if (status) { //Recursive method works until status became false.
             if (methodNumber <= 0) {
-                status = true;
+                status = false; //Status became false when the method number is 0 or negative.
                 return pattern(methodNumber);
             }
             System.out.print(methodNumber + " ");
-            return pattern(methodNumber - 5);
+            return pattern(methodNumber - 5); //Minus 5 until method number is 0 or negative.
+        } else { //when status became false, we need to add 5 to method number because the turning point of the pattern has been reached.
+            if (methodNumber == inputNumber) { //If method number equals to the input number program should stop.
+                System.out.print(methodNumber + " ");
+                return inputNumber;
+            }
+            System.out.print(methodNumber + " ");
+            return pattern(methodNumber + 5);
         }
     }
 

e126a8fbad96994c93e6fcd88779c731088426d8
yasarcandamli <yasarcandamli@gmail.com>
2024-05-30 22:24:18 +0300

    Comments added

--- src/Week06/Practices/Practice01/Practice01_ReadingAndAddingNumbersFromFile.java
+++ src/Week06/Practices/Practice01/Practice01_ReadingAndAddingNumbersFromFile.java
@@ -7,17 +7,18 @@ public class Practice01_ReadingAndAddingNumbersFromFile {
         int sum = 0;
 
         try {
-            FileReader fileReader = new FileReader("./src/Week06/Practices/Practice01/Numbers.txt");
+            FileReader fileReader = new FileReader("./src/Week06/Practices/Practice01/Numbers.txt"); //Reading the file
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
             String line;
-            while ((line = bufferedReader.readLine()) != null) {
+            while ((line = bufferedReader.readLine()) != null) { //Reading the lines in the file and adding up the every line in integer data type
                 int number = Integer.parseInt(line);
                 sum += number;
             }
-            System.out.println(sum);
+            System.out.println(sum); //Printing the sum
 
             fileReader.close();
+            bufferedReader.close();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }

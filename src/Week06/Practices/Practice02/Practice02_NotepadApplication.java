28daa67c40e19c9e57871fd6f4e8538fcf6d235d
yasarcandamli <yasarcandamli@gmail.com>
2024-05-31 17:02:15 +0300

    Codes edited

--- src/Week06/Practices/Practice02/Practice02_NotepadApplication.java
+++ src/Week06/Practices/Practice02/Practice02_NotepadApplication.java
@@ -4,27 +4,30 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Practice02_NotepadApplication {
-    public static void main(String[] args) {
-        String notesPath = "./src/week06/Practices/Practice02/Notes.txt";
-        //Reading the last saved text
+    private static final String notesPath = "./src/week06/Practices/Practice02/Notes.txt";
+
+    //Reading the text
+    private static void readFile() {
         try {
             FileReader fileReader = new FileReader(notesPath);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String line = bufferedReader.readLine();
-            System.out.println("Last saved text: " + line);
+            System.out.println(line);
 
             fileReader.close();
             bufferedReader.close();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+    }
 
+    //Writing the text to Notes.txt
+    private static void writeToFile() {
         //Taking the text that the user wants to save
         Scanner input = new Scanner(System.in);
         System.out.print("Please, enter the text you want to save: ");
         String text = input.nextLine();
 
-        //Writing the text to Notes.txt
         try {
             FileWriter fileWriter = new FileWriter(notesPath);
             PrintWriter printWriter = new PrintWriter(fileWriter);
@@ -35,18 +38,13 @@ public class Practice02_NotepadApplication {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
 
-        //Reading the text written by the user
-        try {
-            FileReader fileReader = new FileReader(notesPath);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String line = bufferedReader.readLine();
-            System.out.println("The text you saved: " + line);
-
-            fileReader.close();
-            bufferedReader.close();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+    public static void main(String[] args) {
+        System.out.print("Last saved text: ");
+        readFile(); //Reading the text inside the file
+        writeToFile(); //Writing the text to Notes.txt
+        System.out.print("The text you saved: ");
+        readFile(); //Reading the text that written by the user
     }
 }

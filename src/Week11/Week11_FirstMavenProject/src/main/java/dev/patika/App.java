84469170937e643855e8de0a6bdbd3e0c70faf2c
yasarcandamli <yasarcandamli@gmail.com>
2024-07-05 10:44:17 +0300

    Entity ve veritabanı ilişkisi

--- src/Week11/Week11_FirstMavenProject/src/main/java/dev/patika/App.java
+++ src/Week11/Week11_FirstMavenProject/src/main/java/dev/patika/App.java
@@ -8,6 +8,5 @@ public class App {
     public static void main(String[] args) {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("market");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
-        System.out.println("test");
     }
 }
---
+++ src/Week11/Week11_FirstMavenProject/src/main/java/dev/patika/Customer.java
@@ -0,0 +1,73 @@
+package dev.patika;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table (name = "customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id", columnDefinition = "serial")
+    private Integer customerId;
+
+    @Column(name = "customer_name", length = 100, nullable = false)
+    private String customerName;
+
+    @Column(name = "customer_mail", unique = true, nullable = false)
+    private String customerMail;
+
+    @Column(name = "customer_on_date")
+    @Temporal(TemporalType.DATE)
+    private LocalDate customerOnDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "customer_gender")
+    private Gender customerGender;
+
+    public enum Gender {
+        MALE,
+        FEMALE
+    }
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerMail() {
+        return customerMail;
+    }
+
+    public void setCustomerMail(String customerMail) {
+        this.customerMail = customerMail;
+    }
+
+    public LocalDate getCustomerOnDate() {
+        return customerOnDate;
+    }
+
+    public void setCustomerOnDate(LocalDate customerOnDate) {
+        this.customerOnDate = customerOnDate;
+    }
+
+    public Gender getCustomerGender() {
+        return customerGender;
+    }
+
+    public void setCustomerGender(Gender customerGender) {
+        this.customerGender = customerGender;
+    }
+}

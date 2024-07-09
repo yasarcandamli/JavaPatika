e4968126417b8861fc460c1e26040b7348e40f80
yasarcandamli <yasarcandamli@gmail.com>
2024-07-09 23:05:38 +0300

    IoC Uygulaması

--- src/Week12/SpringBootKurulum/src/main/java/dev/patika/spring/api/CustomerController.java
+++ src/Week12/SpringBootKurulum/src/main/java/dev/patika/spring/api/CustomerController.java
@@ -2,6 +2,7 @@ package dev.patika.spring.api;
 
 import dev.patika.spring.Customer;
 import dev.patika.spring.business.abstracts.ICustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,14 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
-    private final ICustomerService customerService;
 
+    //Constructor
+    private final ICustomerService customerService;
+    @Autowired
     public CustomerController(ICustomerService customerService) {
         this.customerService = customerService;
     }
 
-    @GetMapping("/{id}")
-    Customer getById(@PathVariable("id") int id) {
-        return this.customerService.getById(id);
+    //Setter
+    private ICustomerService customerService;
+    @Autowired
+    public void setCustomerService(ICustomerService customerService) {
+        this.customerService = customerService;
     }
+
+    @Autowired
+    private ICustomerService customerService;
 }
